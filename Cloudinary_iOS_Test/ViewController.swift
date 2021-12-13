e6bd1a3d686544d8e0b97cacc5f1eaa54c99e9b3
Adi Mizrahi <adi.mizrahi@cloudinary.com>
2021-12-13 08:48:10 +0200

    fixed upload function added URL generate function added download image function.

--- Cloudinary_iOS_Test/ViewController.swift
+++ Cloudinary_iOS_Test/ViewController.swift
@@ -9,26 +9,56 @@ import UIKit
 import Cloudinary
 class ViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var originalImageView: UIImageView!
+    var cloudinary: CLDCloudinary!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let image = UIImage(named: "test") else {
+            return
+        }
+        originalImageView.frame = CGRect(x: originalImageView.bounds.origin.x, y: originalImageView.bounds.origin.y, width: 240, height: 240 * image.size.width / image.size.height)
+        originalImageView.image = image
         // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        uploadImageToCloudinary()
+        initCloudinary()
+            //uploadImageToCloudinary()
+        generateURL()
+    }
+    
+    func initCloudinary() {
+        let config = CLDConfiguration(cloudName: "db0edm7to")
+        cloudinary = CLDCloudinary(configuration: config)
+        CLDCloudinary.logLevel = CLDLogLevel.debug
+    }
+    
+    func generateURL() {
+        let transformation = CLDTransformation().setCrop(.fit).setRadius(50)
+        guard let url = cloudinary.createUrl().setTransformation(transformation).generate("corgi.png") else {
+            return
+        }
+        downloadImage(url)
+    }
+    
+    func downloadImage(_ url: String) {
+        cloudinary.createDownloader().fetchImage(url, nil, completionHandler: {  image, error in
+            DispatchQueue.main.async {
+                self.imageView.image = image
+            }
+        })
     }
     
     func uploadImageToCloudinary() {
-        let config = CLDConfiguration(cloudName: "db0edm7to", secure: true)
-        let cloudinary = CLDCloudinary(configuration: config)
         
-        
-        let params: CLDUploadRequestParams = CLDUploadRequestParams().setUploadPreset("sample").setPublicId("newImage14").setFolder("").setResourceType(.image) as! CLDUploadRequestParams
+        let params: CLDUploadRequestParams = CLDUploadRequestParams().setUploadPreset("boj7jh4o").setPublicId("newImage14").setFolder("").setResourceType(.image) as! CLDUploadRequestParams
         guard let data = UIImage(named: "test")?.pngData() else {
             return
         }
-        let request = cloudinary.createUploader().signedUpload(data: data) { response, error in
+        let request = cloudinary.createUploader().upload(data: data, uploadPreset: "boj7jh4o") { response, error in
             if let error = error {
                 print("error: \(error.localizedDescription)")
             } else {
